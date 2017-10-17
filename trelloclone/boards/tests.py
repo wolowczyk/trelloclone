853425e39eba9545de1617e65d24ffdc3ae853fe
Pawel Wolowczyk <pawelwolowczyk@gmail.com>
2017-10-17 16:14:27 +0200

    updates

--- trelloclone/boards/tests.py
+++ trelloclone/boards/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from .models import List, Card
 
-# Create your tests here.
+
+class ListTestCase(TestCase):
+    def setUp(self):
+        List.objects.create(list_name="Ballroom")
+        List.objects.create(list_name="Animals")
+
+
+class CardTestCase(TestCase):
+    def setUp(self):
+        Card.objects.create(card_title="Tables", card_description="There are so many tables there", card_list=List.objects.first())
+        Card.objects.create(list_name="Dances", card_description="And they are dancing a lot", card_list=List.objects.first())
